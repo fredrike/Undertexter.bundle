541d651d720e4508ad698cabccbf28240f816566
James Clarke <james@plexapp.com>
2010-08-04 01:59:54 +0100

    Initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,42 @@
+#opensubtitles.org
+#Subtitles service allowed by www.OpenSubtitles.org
+
+OS_API = 'http://api.opensubtitles.org/xml-rpc'
+OS_LANGUAGE_CODES = 'http://www.opensubtitles.org/addons/export_languages.php'
+OS_PLEX_USERAGENT = 'plexapp.com v9.0'
+
+def Start():
+  HTTP.CacheTime = CACHE_1DAY
+  HTTP.Headers['User-agent'] = 'plexapp.com v9.0'
+
+@expose
+def GetImdbIdFromHash(openSubtitlesHash, lang):
+  proxy = XMLRPC.Proxy(OS_API)
+  try:
+    os_movieInfo = proxy.CheckMovieHash('',[openSubtitlesHash])
+  except:
+    return None
+    
+  if os_movieInfo['data'][openSubtitlesHash] != []:
+    return MetadataSearchResult(
+      id    = "tt" + str(os_movieInfo['data'][openSubtitlesHash]['MovieImdbID']),
+      name  = str(os_movieInfo['data'][openSubtitlesHash]['MovieName']),
+      year  = int(os_movieInfo['data'][openSubtitlesHash]['MovieYear']),
+      lang  = lang,
+      score = 90)
+  else:
+    return None
+  
+class OpenSubtitlesAgent(Agent.Movies):
+  name = 'OpenSubtitles.org'
+  languages = [Locale.Language.English]
+  primary_provider = False
+  contributes_to = ['com.plexapp.agents.imdb']
+  
+  def search(self, results, media, lang):
+    match = GetImdbIdFromHash(media.openSubtitlesHash, lang)
+    if match is not None:
+      results.Append(match)
+
+  def update(self, metadata, lang):
+    pass
